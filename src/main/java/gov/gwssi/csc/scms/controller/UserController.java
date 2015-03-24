a08add4cb9de1731c07dcf9e5a50b7f6d2da3d84
WangZishi <ynh.2@outlook.com>
2015-03-24 15:52:53 +0800

    强迫症犯了 硬是把JAVA写出了Python的味道

--- src/main/java/gov/gwssi/csc/scms/controller/UserController.java
+++ src/main/java/gov/gwssi/csc/scms/controller/UserController.java
@@ -1,9 +1,9 @@
-package gov.gwssi.csc.scms.controller;
+package gov.gwssi.csc.scms.controller                                          ;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable                    ;
+import org.springframework.web.bind.annotation.RequestMapping                  ;
+import org.springframework.web.bind.annotation.RequestMethod                   ;
+import org.springframework.web.bind.annotation.RestController                  ;
 
 
 /**
@@ -11,23 +11,70 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController("userController")
 @RequestMapping("/service/user")
-public class UserController {
-//    UserService userService=new UserService();
+public class UserController                                                    {
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET,headers="Accept=application/json")
-    public String getUser(@PathVariable int id) {
-        String user = id + "";
-        return user;
-    }
+    @RequestMapping                                                            (
+        value   = "/{id}"                                                      ,
+        method  = RequestMethod.GET                                            ,
+        headers = "Accept=application/json"                                    )
+    public String
+    getUser(@PathVariable int id)                                              {
+        String user = id + ""                                                  ;
+        return user                                                           ;}
 
-    @RequestMapping(method = RequestMethod.GET,headers="Accept=application/json")
-    public String getAllUsers() {
-        String users = "[{\"cscNumber\":\"CSC20150101\",\"ppName\": \"BRUCE WAYNE\",\"nationality\": \"United States\",\"dispatchWay\": \"way one\",\"placeProperties\": \"pro one\",\"projectType\": \"type one\",\"projectName\": \"name one\"},\n" +
-                "{\"cscNumber\":\"CSC20150102\",\"ppName\": \"BRUCE WAYNE TWO\",\"nationality\": \"United States\",\"dispatchWay\": \"way two\",\"placeProperties\": \"pro two\",\"projectType\": \"type two\",\"projectName\": \"name two\"},\n" +
-                "{\"cscNumber\":\"CSC20150103\",\"ppName\": \"BRUCE WAYNE THREE\",\"nationality\": \"United States\",\"dispatchWay\": \"way three\",\"placeProperties\": \"pro three\",\"projectType\": \"type three\",\"projectName\": \"name three\"},\n" +
-                "{\"cscNumber\":\"CSC20150104\",\"ppName\": \"BRUCE WAYNE FOUR\",\"nationality\": \"United States\",\"dispatchWay\": \"way four\",\"placeProperties\": \"pro four\",\"projectType\": \"type four\",\"projectName\": \"name four\"},\n" +
-                "{\"cscNumber\":\"CSC20150105\",\"ppName\": \"BRUCE WAYNE FIVE\",\"nationality\": \"United States\",\"dispatchWay\": \"way five\",\"placeProperties\": \"pro five\",\"projectType\": \"type five\",\"projectName\": \"name five\"},\n" +
-                "{\"cscNumber\":\"CSC20150106\",\"ppName\": \"BRUCE WAYNE SIX\",\"nationality\": \"United States\",\"dispatchWay\": \"way six\",\"placeProperties\": \"pro six\",\"projectType\": \"type six\",\"projectName\": \"name six\"}]";
-        return users;
-    }
-}
+    @RequestMapping                                                            (
+        method  = RequestMethod.GET                                            ,
+        headers = "Accept=application/json"                                    )
+    public String
+    getAllUsers()                                                              {
+        String users =
+            "[{"                                                               +
+                "\"cscNumber\"      : \"CSC20150101\","                        +
+                "\"ppName\"         : \"BRUCE WAYNE\","                        +
+                "\"nationality\"    : \"United States\","                      +
+                "\"dispatchWay\"    : \"way one\","                            +
+                "\"placeProperties\": \"pro one\","                            +
+                "\"projectType\"    : \"type one\","                           +
+                "\"projectName\"    : \"name one\""                            +
+            "},{"                                                              +
+                "\"cscNumber\"      : \"CSC20150102\","                        +
+                "\"ppName\"         : \"BRUCE WAYNE TWO\","                    +
+                "\"nationality\"    : \"United States\","                      +
+                "\"dispatchWay\"    : \"way two\","                            +
+                "\"placeProperties\": \"pro two\","                            +
+                "\"projectType\"    : \"type two\","                           +
+                "\"projectName\"    : \"name two\""                            +
+            "},{"                                                              +
+                "\"cscNumber\"      : \"CSC20150103\","                        +
+                "\"ppName\"         : \"BRUCE WAYNE THREE\","                  +
+                "\"nationality\"    : \"United States\","                      +
+                "\"dispatchWay\"    : \"way three\","                          +
+                "\"placeProperties\": \"pro three\","                          +
+                "\"projectType\"    : \"type three\","                         +
+                "\"projectName\"    : \"name three\""                          +
+            "},{"                                                              +
+                "\"cscNumber\"      : \"CSC20150104\","                        +
+                "\"ppName\"         : \"BRUCE WAYNE FOUR\","                   +
+                "\"nationality\"    : \"United States\","                      +
+                "\"dispatchWay\"    : \"way four\","                           +
+                "\"placeProperties\": \"pro four\","                           +
+                "\"projectType\"    : \"type four\","                          +
+                "\"projectName\"    : \"name four\""                           +
+            "},{"                                                              +
+                "\"cscNumber\"      : \"CSC20150105\","                        +
+                "\"ppName\"         : \"BRUCE WAYNE FIVE\","                   +
+                "\"nationality\"    : \"United States\","                      +
+                "\"dispatchWay\"    : \"way five\","                           +
+                "\"placeProperties\": \"pro five\","                           +
+                "\"projectType\"    : \"type five\","                          +
+                "\"projectName\"    : \"name five\""                           +
+            "},{"                                                              +
+                "\"cscNumber\"      : \"CSC20150106\","                        +
+                "\"ppName\"         : \"BRUCE WAYNE SIX\","                    +
+                "\"nationality\"    : \"United States\","                      +
+                "\"dispatchWay\"    : \"way six\","                            +
+                "\"placeProperties\": \"pro six\","                            +
+                "\"projectType\"    : \"type six\","                           +
+                "\"projectName\"    : \"name six\""                            +
+            "}]"                                                               ;
+        return users                                                         ;}}
