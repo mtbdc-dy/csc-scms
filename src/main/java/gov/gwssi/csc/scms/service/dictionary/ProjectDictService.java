82b0750e0e5e0c7906ff4a64df684c9bc98e3e8a
GloryRoadWang <tyutwang@gmail.com>
2015-04-24 10:01:58 +0800

    add ProjectDictService class

---
+++ src/main/java/gov/gwssi/csc/scms/service/dictionary/ProjectDictService.java
@@ -0,0 +1,25 @@
+package gov.gwssi.csc.scms.service.dictionary;
+
+import gov.gwssi.csc.scms.dao.dictionary.ProjectDictDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by WangZhenghua on 2015/4/23.
+ * 项目代码表service类
+ */
+
+@Service("projectDictService")
+public class ProjectDictService {
+
+    @Autowired
+    private ProjectDictDAO projectDictDAO;
+
+    // 根据project层次获取相应的项目代码表(1为一级,2为二级)
+    public String getProjectDictJsonData(String level){
+        String jsonData = "";
+        jsonData = projectDictDAO.getProjectDictJsonDataByLevel(level);
+        System.out.println("ProjectDictService-->getProjectDictJsonData方法jsonData == " + jsonData);
+        return jsonData;
+    }
+}
