9ad91988dcefcffd729ab6631acab2c74851807f
GloryRoadWang <tyutwang@gmail.com>
2015-04-24 10:50:34 +0800

    add AbnormalService class

---
+++ src/main/java/gov/gwssi/csc/scms/service/abnormal/AbnormalService.java
@@ -0,0 +1,11 @@
+package gov.gwssi.csc.scms.service.abnormal;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by WangZhenghua on 2015/4/24.
+ */
+
+@Service("abnormaltService")
+public class AbnormalService {
+}
