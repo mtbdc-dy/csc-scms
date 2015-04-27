9442a0e527fbcbdd9732833fcb5517b3843c45ba
GloryRoadWang <tyutwang@gmail.com>
2015-04-27 21:04:21 +0800

    add the TranslateDictJson class

---
+++ src/main/java/gov/gwssi/csc/scms/domain/dictionary/TranslateDictJson.java
@@ -0,0 +1,36 @@
+package gov.gwssi.csc.scms.domain.dictionary;
+
+/**
+ * Created by WangZhenghua on 2015/4/27.
+ * 代码表转义(一层)
+ */
+public class TranslateDictJson {
+
+    private String code;
+    private String value;
+    private String valid;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getValid() {
+        return valid;
+    }
+
+    public void setValid(String valid) {
+        this.valid = valid;
+    }
+}
