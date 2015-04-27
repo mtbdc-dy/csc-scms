3eec1ac421d0f40bca713bd43d53cfef2afda482
GloryRoadWang <tyutwang@gmail.com>
2015-04-27 21:05:53 +0800

    add the TranslateDictServiceTest class

---
+++ src/test/java/gov/gwssi/csc/scms/service/dictionary/TranslateDictServiceTest.java
@@ -0,0 +1,16 @@
+package gov.gwssi.csc.scms.service.dictionary;
+
+import gov.gwssi.csc.scms.base.UnitTestBase;
+import org.junit.Test;
+
+/**
+ * Created by WangZhenghua on 2015/4/27.
+ * 代码表测试用例
+ */
+public class TranslateDictServiceTest  extends UnitTestBase {
+    @Test
+    public void getTranslateDict(){
+        TranslateDictService translateDictService = getBean("translateDictService");
+        translateDictService.getTranslateDictJsonData("B");
+    }
+}
