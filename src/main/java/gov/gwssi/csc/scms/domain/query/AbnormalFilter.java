35bfac5522f27a3946842f9af1eee9161af47f41
Li Zhisheng <lizhisheng@gwssi.com>
2015-04-29 18:03:21 +0800

    添加异动申请查询列表结果类

---
+++ src/main/java/gov/gwssi/csc/scms/domain/query/AbnormalFilter.java
@@ -0,0 +1,49 @@
+package gov.gwssi.csc.scms.domain.query;
+
+import java.util.List;
+
+/**
+ * Created by think on 2015/4/29.
+ */
+public class AbnormalFilter implements Filter{
+    private AbnormalFilterObject filterObject;
+    private List<FilterCell> conditions;
+    public AbnormalFilter(AbnormalFilterObject filterObject) {
+        this.filterObject = filterObject;
+        if (filterObject != null)
+            conditions = filterObject.getConditions();
+    }
+    @Override
+    public String getFilter() {
+        if (conditions == null || conditions.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (FilterCell fc : conditions) {
+            String str[] = fc.getValue().split(",");
+
+            if ("String".equalsIgnoreCase(fc.getType())) {
+                if (str.length > 1) {
+                    sb.append(" and ").append(fc.getTableName()).append(".").append(fc.getColumnName()).append(" in (");
+                    for (String parm : str) {
+                        sb.append("'").append(parm).append("',");
+                    }
+                    sb.setCharAt(sb.length(), ')');
+                } else {
+                    sb.append(" and ").append(fc.getTableName()).append(".").append(fc.getColumnName()).append(" = '").append(str[0]).append("' ");
+                }
+            }
+            if ("Date".equalsIgnoreCase(fc.getType())) {
+                if (str.length > 1) {
+                    sb.append(" and to_char(").append(fc.getTableName()).append(".").append(fc.getColumnName()).append(",'yyyy-mm-dd hh24:mi:ss') between ");
+                    sb.append("'").append(str[0]).append("' and '").append(str[1]).append("' ");
+                } else {
+                    sb.append(" and ").append(fc.getTableName()).append(".").append(fc.getColumnName()).append(" = ").append("to_date('").append(str[0]).append("','yyyy-mm-dd hh24:mi:ss')");
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ src/main/java/gov/gwssi/csc/scms/domain/query/AbnormalResultObject.java
@@ -0,0 +1,150 @@
+package gov.gwssi.csc.scms.domain.query;
+
+import java.util.Date;
+
+/**
+ * Created by lzs on 2015/4/29.
+ * 异动申请管理查询学生信息列表结果类
+ */
+public class AbnormalResultObject extends ResultObject{
+    /**
+     * 学生ID
+     */
+    private Long studentId;
+    /**
+     * CSCID
+     */
+    private String cscId;
+    /**
+     * 性别
+     */
+    private String sex;
+
+    /**
+     * 护照姓名
+     */
+    private String passportName;
+
+    /**
+     * 申请人
+     *
+     */
+    private String applyUserName;
+    /**
+     * 国籍
+     */
+    private String country;
+    /**
+     * 申请时间
+     */
+    private Date applyDate;
+    /**
+     * 学籍状态
+     */
+    private String rollState;
+    /**
+     * 处理状态（ 0未提交 1提交未上报 2上报未审核 3审核未处理 4 处理）
+     */
+    private String handleState;
+
+    public AbnormalResultObject(Long studentId, String cscId, String sex, String passportName, String applyUserName, String country, Date applyDate, String rollState, String handleState) {
+        this.studentId = studentId;
+        this.cscId = cscId;
+        this.sex = sex;
+        this.passportName = passportName;
+        this.applyUserName = applyUserName;
+        this.country = country;
+        this.applyDate = applyDate;
+        this.rollState = rollState;
+        this.handleState = handleState;
+    }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getCscId() {
+        return cscId;
+    }
+
+    public void setCscId(String cscId) {
+        this.cscId = cscId;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPassportName() {
+        return passportName;
+    }
+
+    public void setPassportName(String passportName) {
+        this.passportName = passportName;
+    }
+
+    public String getApplyUserName() {
+        return applyUserName;
+    }
+
+    public void setApplyUserName(String applyUserName) {
+        this.applyUserName = applyUserName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Date getApplyDate() {
+        return applyDate;
+    }
+
+    public void setApplyDate(Date applyDate) {
+        this.applyDate = applyDate;
+    }
+
+    public String getRollState() {
+        return rollState;
+    }
+
+    public void setRollState(String rollState) {
+        this.rollState = rollState;
+    }
+
+    public String getHandleState() {
+        return handleState;
+    }
+
+    public void setHandleState(String handleState) {
+        this.handleState = handleState;
+    }
+    public static String getResultObject() {
+        /**
+         *  this.studentId = studentId;
+         this.cscId = cscId;
+         this.sex = sex;
+         this.passportName = passportName;
+         this.applyUserName = applyUserName;
+         this.country = country;
+         this.applyDate = applyDate;
+         this.rollState = rollState;
+         this.handleState = handleState;
+         */
+        String resultSql = "select new gov.gwssi.csc.scms.domain.query.AbnormalResultObject(student.id, student.cscId, basicInfo.sex, " +
+                "basicInfo.passportName, " +
+                "basicInfo.country, schoolRoll.rollState,abnormal.handleState,abnormal.applyUserName,abnormal.applyDate) ";
+        return resultSql;
+    }
+}
