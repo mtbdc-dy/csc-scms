7749c1a669b5effa99357c9b8bea252df7b409d0
Li Zhisheng <lizhisheng@gwssi.com>
2015-05-04 09:13:22 +0800

    将默认值设置成静态变量

--- src/main/java/gov/gwssi/csc/scms/domain/query/FilterObject.java
+++ src/main/java/gov/gwssi/csc/scms/domain/query/FilterObject.java
@@ -13,7 +13,25 @@ public abstract class FilterObject {
     public static final int OFFSETDEFULT = 0;//始值
     public static final int PAGESIZEDEFULT = 200;//页值
 
+    private String offSet = "0";
 
+    private String pageSize = "200";
+
+    public String getOffSet() {
+        return offSet;
+    }
+
+    public void setOffSet(String offSet) {
+        this.offSet = offSet;
+    }
+
+    public String getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(String pageSize) {
+        this.pageSize = pageSize;
+    }
     abstract List getConditions();
 
     protected List<FilterCell> addCondition(List<FilterCell> conditions, String tableName, String filedName, String type, String value) {
