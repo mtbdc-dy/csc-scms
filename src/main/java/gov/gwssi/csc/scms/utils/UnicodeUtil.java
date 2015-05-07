a2f68680b041cdd2c579f9098688889ff1626b7a
Murray <378516602@qq.com>
2015-05-07 21:05:44 +0800

    fix

--- src/main/java/gov/gwssi/csc/scms/utils/UnicodeUtil.java
+++ src/main/java/gov/gwssi/csc/scms/utils/UnicodeUtil.java
@@ -2,13 +2,14 @@ package gov.gwssi.csc.scms.utils;
 
 import org.springframework.util.StringUtils;
 
-import java.io.UnsupportedEncodingException;
-
 /**
  * Created by Lei on 2015/5/7.
  */
 public class UnicodeUtil {
     public static String toUNICODE(String s) {
+        if (StringUtils.isEmpty(s))
+            return "";
+
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < s.length(); i++) {
             //ASCII表中的字符码值不够4位,补00
@@ -23,6 +24,8 @@ public class UnicodeUtil {
     }
 
     public static String toCharSequence(String unicodeValues) {
+        if (StringUtils.isEmpty(unicodeValues))
+            return "";
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < unicodeValues.length() - 4; i++) {
             if (unicodeValues.charAt(i) == '\\') {
