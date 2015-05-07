3252979c35e741717437834df5bf79fca55205c1
Li Zhisheng <lizhisheng@gwssi.com>
2015-05-07 17:20:29 +0800

    添加异动申请页面列表所需的所有字段

--- src/main/java/gov/gwssi/csc/scms/domain/query/AbnormalResultObject.java
+++ src/main/java/gov/gwssi/csc/scms/domain/query/AbnormalResultObject.java
@@ -18,7 +18,7 @@ public class AbnormalResultObject extends ResultObject{
     /**
      * 性别
      */
-    private String sex;
+    private String gender;
 
     /**
      * 护照姓名
@@ -31,13 +31,15 @@ public class AbnormalResultObject extends ResultObject{
      */
     private String applyUserName;
     /**
+     * 申请时间
+     *
+     */
+    private Date applyTime;
+    /**
      * 国籍
      */
     private String country;
-    /**
-     * 申请时间
-     */
-    private Date applyDate;
+
     /**
      * 学籍状态
      */
@@ -46,17 +48,252 @@ public class AbnormalResultObject extends ResultObject{
      * 处理状态（ 0未提交 1提交未上报 2上报未审核 3审核未处理 4 处理）
      */
     private String handleState;
+    //新增字段
+    /**
+     * 原因id
+     */
+    private String reasonId;
+    /**
+     * 原因
+     */
+    private String reason;
+    /**
+     * 异动上传pdf文件路径
+     */
+    private String applyUri;
+    /**
+     * 审批结果 0不同意 1同意
+     */
+    private String approResult;
+    /**
+     * 审批意见
+     */
+    private String approOpinion;
+    /**
+     * 发文pdf文件路径
+     */
+    private String publicUri;
 
-    public AbnormalResultObject(String studentId, String cscId, String sex, String passportName, String applyUserName, String country, Date applyDate, String rollState, String handleState) {
+    /**
+     *  申请人ID
+     */
+    private String applyUserId;
+
+    /**
+     * 上报人ID
+     */
+    private String reportUserId;
+    /**
+     * 上报人
+     */
+    private String reportUserName;
+    /**
+     * 上报日期
+     */
+    private Date reportTime;
+    /**
+     * 审批人ID
+     */
+    private String approUserId;
+    /**
+     * 审批人
+     */
+    private String approUserName;
+    /**
+     * 审批日期
+     */
+    private Date approTime;
+    /**
+     * 处理人ID
+     */
+    private String handleUserId;
+    /**
+     * 处理人
+     */
+    private String handleUserName;
+    /**
+     * 处理日期
+     */
+    private Date handleTime;
+
+    public AbnormalResultObject() {
+    }
+
+    public AbnormalResultObject(String studentId, String cscId, String gender, String passportName,
+                                String applyUserName, Date applyTime, String country, String rollState, String handleState,
+                                String reasonId, String reason, String applyUri, String approResult, String approOpinion,
+                                String publicUri, String applyUserId, String reportUserId, String reportUserName,
+                                Date reportTime, String approUserId, String approUserName, Date approTime,
+                                String handleUserId, String handleUserName, Date handleTime) {
         this.studentId = studentId;
         this.cscId = cscId;
-        this.sex = sex;
+        this.gender = gender;
         this.passportName = passportName;
         this.applyUserName = applyUserName;
+        this.applyTime = applyTime;
         this.country = country;
-        this.applyDate = applyDate;
         this.rollState = rollState;
         this.handleState = handleState;
+        this.reasonId = reasonId;
+        this.reason = reason;
+        this.applyUri = applyUri;
+        this.approResult = approResult;
+        this.approOpinion = approOpinion;
+        this.publicUri = publicUri;
+        this.applyUserId = applyUserId;
+        this.reportUserId = reportUserId;
+        this.reportUserName = reportUserName;
+        this.reportTime = reportTime;
+        this.approUserId = approUserId;
+        this.approUserName = approUserName;
+        this.approTime = approTime;
+        this.handleUserId = handleUserId;
+        this.handleUserName = handleUserName;
+        this.handleTime = handleTime;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getApplyTime() {
+        return applyTime;
+    }
+
+    public void setApplyTime(Date applyTime) {
+        this.applyTime = applyTime;
+    }
+
+    public String getReasonId() {
+        return reasonId;
+    }
+
+    public void setReasonId(String reasonId) {
+        this.reasonId = reasonId;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getApplyUri() {
+        return applyUri;
+    }
+
+    public void setApplyUri(String applyUri) {
+        this.applyUri = applyUri;
+    }
+
+    public String getApproResult() {
+        return approResult;
+    }
+
+    public void setApproResult(String approResult) {
+        this.approResult = approResult;
+    }
+
+    public String getApproOpinion() {
+        return approOpinion;
+    }
+
+    public void setApproOpinion(String approOpinion) {
+        this.approOpinion = approOpinion;
+    }
+
+    public String getPublicUri() {
+        return publicUri;
+    }
+
+    public void setPublicUri(String publicUri) {
+        this.publicUri = publicUri;
+    }
+
+    public String getApplyUserId() {
+        return applyUserId;
+    }
+
+    public void setApplyUserId(String applyUserId) {
+        this.applyUserId = applyUserId;
+    }
+
+    public String getReportUserId() {
+        return reportUserId;
+    }
+
+    public void setReportUserId(String reportUserId) {
+        this.reportUserId = reportUserId;
+    }
+
+    public String getReportUserName() {
+        return reportUserName;
+    }
+
+    public void setReportUserName(String reportUserName) {
+        this.reportUserName = reportUserName;
+    }
+
+    public Date getReportTime() {
+        return reportTime;
+    }
+
+    public void setReportTime(Date reportTime) {
+        this.reportTime = reportTime;
+    }
+
+    public String getApproUserId() {
+        return approUserId;
+    }
+
+    public void setApproUserId(String approUserId) {
+        this.approUserId = approUserId;
+    }
+
+    public String getApproUserName() {
+        return approUserName;
+    }
+
+    public void setApproUserName(String approUserName) {
+        this.approUserName = approUserName;
+    }
+
+    public Date getApproTime() {
+        return approTime;
+    }
+
+    public void setApproTime(Date approTime) {
+        this.approTime = approTime;
+    }
+
+    public String getHandleUserId() {
+        return handleUserId;
+    }
+
+    public void setHandleUserId(String handleUserId) {
+        this.handleUserId = handleUserId;
+    }
+
+    public String getHandleUserName() {
+        return handleUserName;
+    }
+
+    public void setHandleUserName(String handleUserName) {
+        this.handleUserName = handleUserName;
+    }
+
+    public Date getHandleTime() {
+        return handleTime;
+    }
+
+    public void setHandleTime(Date handleTime) {
+        this.handleTime = handleTime;
     }
 
     public String getStudentId() {
@@ -75,13 +312,7 @@ public class AbnormalResultObject extends ResultObject{
         this.cscId = cscId;
     }
 
-    public String getSex() {
-        return sex;
-    }
 
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
 
     public String getPassportName() {
         return passportName;
@@ -107,13 +338,6 @@ public class AbnormalResultObject extends ResultObject{
         this.country = country;
     }
 
-    public Date getApplyDate() {
-        return applyDate;
-    }
-
-    public void setApplyDate(Date applyDate) {
-        this.applyDate = applyDate;
-    }
 
     public String getRollState() {
         return rollState;
@@ -132,19 +356,19 @@ public class AbnormalResultObject extends ResultObject{
     }
     public static String getResultObject() {
         /**
-         *  this.studentId = studentId;
-         this.cscId = cscId;
-         this.sex = sex;
-         this.passportName = passportName;
-         this.applyUserName = applyUserName;
-         this.country = country;
-         this.applyDate = applyDate;
-         this.rollState = rollState;
-         this.handleState = handleState;
+         * String studentId, String cscId, String gender, String passportName,
+         String applyUserName, Date applyTime, String country, String rollState, String handleState,
+         String reasonId, String reason, String applyUri, String approResult, String approOpinion,
+         String publicUri, String applyUserId, String reportUserId, String reportUserName,
+         Date reportTime, String approUserId, String approUserName, Date approTime,
+         String handleUserId, String handleUserName, Date handleTime
          */
         String resultSql = "select new gov.gwssi.csc.scms.domain.query.AbnormalResultObject(student.id, student.cscId, basicInfo.gender, " +
                 "basicInfo.passportName, " +
-                "abnormal.applyUserName,basicInfo.country,abnormal.applyTime, schoolRoll.state,abnormal.state) ";
+                "abnormal.applyUserName,abnormal.applyTime,basicInfo.country,schoolRoll.state,abnormal.state,abnormal.reasonId," +
+                " abnormal.reason,abnormal.applyUri,abnormal.approResult,abnormal.approOpinion,abnormal.publicUri,abnormal.applyUserId,abnormal.reportUserId," +
+                "abnormal.reportUserName,abnormal.reportTime,abnormal.approUserId,abnormal.approUserName,abnormal.approTime,abnormal.handleUserId," +
+                "abnormal.handleUserName,abnormal.handleTime) ";
         return resultSql;
     }
 }
