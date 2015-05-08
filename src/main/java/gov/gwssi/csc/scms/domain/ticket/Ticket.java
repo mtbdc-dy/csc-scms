a8cb9e9d078d5672a2d5cc30031b4f61cc3908b1
Li Zhisheng <lizhisheng@gwssi.com>
2015-05-08 10:41:33 +0800

    修改机票domain类，和数据库保持一致

--- src/main/java/gov/gwssi/csc/scms/domain/ticket/Ticket.java
+++ src/main/java/gov/gwssi/csc/scms/domain/ticket/Ticket.java
@@ -1,6 +1,7 @@
 package gov.gwssi.csc.scms.domain.ticket;
 
 import gov.gwssi.csc.scms.domain.scholarship.ScholarshipDetail;
+import gov.gwssi.csc.scms.domain.student.Student;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -15,62 +16,212 @@ import java.util.List;
 @Table(name = "SCMS_AIRTICKET")
 public class Ticket {
     @Id
-    @SequenceGenerator(name = "SCMS_AIRTICKET_ID",sequenceName = "SCMS_AIRTICKET_SEQ",allocationSize = 1)
-    @GeneratedValue(generator = "SCMS_AIRTICKET_ID",strategy = GenerationType.SEQUENCE)
-    private Long id;
 
+    private String id;
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "studentid")
+    private Student student;
     /**
      *院校 可以不需要 待定
      */
-    @Column(name = "school",length=4)
+    @Column(name = "school",length=6)
     private String school;
 
     /**
-     *总人数
+     *机票类型 0来华 1回国
      */
-    @Column(name = "totalNum",length=8)
-    private Long totalNum;
+    @Column(name = "type",length=1)
+    private String type;
+    /**
+     *航线
+     */
+    @Column(name = "airLine",length=100)
+    private String airLine;
+    /**
+     *机票号码
+     */
+    @Column(name = "ticketNo",length=100)
+    private String ticketNo;
+    /**
+     *申请乘机日期
+     */
+    @Column(name = "applyDate")
+    private Date applyDate;
+    /**
+     *乘机日期
+     */
+    @Column(name = "flightDate")
+    private Date flightDate;
+    /**
+     *离境城市
+     */
+    @Column(name = "leaveCity",length=100)
+    private String leaveCity;
+    /**
+     *机票价格（元）
+     */
+    @Column(name = "price",length=16)
+    private Double price;
+    /**
+     *订票状态 0未导出 1已导出 2已反馈
+     */
+    @Column(name = "state",length=1)
+    private String state;
 
     /**
-     *导出人数
+     *备注
      */
-    @Column(name = "expNum",length=8)
-    private Long expNum;
+    @Column(name = "remark",length=200)
+    private String remark;
 
     /**
-     *未导出人数
+     *创建人
      */
-    @Column(name = "unExpNum",length=8)
-    private Long unExpNum;
+    @Column(name = "createBy",length=20)
+    private String createBy;
 
     /**
-     *反馈人数
+     *创建时间
      */
-    @Column(name = "backNum",length=8)
-    private Long backNum;
-
+    @Column(name = "created")
+    private Date created;
     /**
-     *用户ID
+     *创建人
      */
-    @Column(name = "userId",length=20)
-    private String userId;
-
-    /**
-     *用户名
-     */
-    @Column(name = "userName",length=50)
+    @Column(name = "updateBy",length=20)
     private String userName;
 
     /**
      *创建时间
      */
-    @Column(name = "time")
-    private Date time;
+    @Column(name = "updated")
+    private Date updated;
 
-    /**
-     * 机票明细
-     */
-    @OneToMany(fetch = FetchType.EAGER)
-    @JoinColumn(name = "ticketid")
-    private List<TicketDetail> ticketDetail = new ArrayList<TicketDetail>();
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAirLine() {
+        return airLine;
+    }
+
+    public void setAirLine(String airLine) {
+        this.airLine = airLine;
+    }
+
+    public String getTicketNo() {
+        return ticketNo;
+    }
+
+    public void setTicketNo(String ticketNo) {
+        this.ticketNo = ticketNo;
+    }
+
+    public Date getApplyDate() {
+        return applyDate;
+    }
+
+    public void setApplyDate(Date applyDate) {
+        this.applyDate = applyDate;
+    }
+
+    public Date getFlightDate() {
+        return flightDate;
+    }
+
+    public void setFlightDate(Date flightDate) {
+        this.flightDate = flightDate;
+    }
+
+    public String getLeaveCity() {
+        return leaveCity;
+    }
+
+    public void setLeaveCity(String leaveCity) {
+        this.leaveCity = leaveCity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
 }
