32e3e7a79ad9f65734635f0d906b6743f86bf176
Li Zhisheng <lizhisheng@gwssi.com>
2015-05-12 23:33:21 +0800

    修改异动删除方法，并完成单元测试

--- src/main/java/gov/gwssi/csc/scms/service/scholarship/ScholarshipService.java
+++ src/main/java/gov/gwssi/csc/scms/service/scholarship/ScholarshipService.java
@@ -1,10 +1,14 @@
 package gov.gwssi.csc.scms.service.scholarship;
 
+import gov.gwssi.csc.scms.service.BaseService;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by Wang Rui on 2015/4/14.
  *
  */
-public class ScholarshipService {
+@Service("scholarshipService")
+public class ScholarshipService extends BaseService {
 
 
 }
