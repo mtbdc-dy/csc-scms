c3e198674a83a0f6870dacee38425382582f48d3
GloryRoadWang <tyutwang@gmail.com>
2015-05-14 20:51:28 +0800

    新建动态查询统计表头Service类DHeaderService

---
+++ src/main/java/gov/gwssi/csc/scms/service/statistics/DHeaderService.java
@@ -0,0 +1,19 @@
+package gov.gwssi.csc.scms.service.statistics;
+
+import gov.gwssi.csc.scms.domain.statistics.DHeader;
+import gov.gwssi.csc.scms.repository.statistics.DHeaderRepository;
+import gov.gwssi.csc.scms.service.BaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by WangZhenghua on 2015/5/14.
+ * 动态查询统计表头Service层
+ */
+@Service("dHeaderService")
+public class DHeaderService extends BaseService{
+
+}
