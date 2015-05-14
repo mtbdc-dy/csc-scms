4c66c0d0e68e25cdfcad32042c27eaf000cd84a1
GloryRoadWang <tyutwang@gmail.com>
2015-05-14 20:35:59 +0800

    修改AppConfig类中basePackages数组的值

--- src/main/java/gov/gwssi/csc/scms/configuration/AppConfig.java
+++ src/main/java/gov/gwssi/csc/scms/configuration/AppConfig.java
@@ -12,18 +12,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 /**
  * Created by WangZishi on 3/27/2015.
- *
+ * <p/>
  * Spring Bean的Java Config。
  */
 @EnableWebMvc
 @ComponentScan(
-    basePackages = {
-        "gov.gwssi.csc.scms.controller",
-        "gov.gwssi.csc.scms.service",
-        "gov.gwssi.csc.scms.repository",
-        "gov.gwssi.csc.scms.dao",
-        "gov.gwssi.csc.scms.domain.user"
-    }
+        basePackages = {
+                "gov.gwssi.csc.scms.controller",
+                "gov.gwssi.csc.scms.service",
+                "gov.gwssi.csc.scms.repository",
+                "gov.gwssi.csc.scms.dao",
+                "gov.gwssi.csc.scms.domain"
+        }
 )
 @Import(DatabaseConfig.class)
 @Configuration
