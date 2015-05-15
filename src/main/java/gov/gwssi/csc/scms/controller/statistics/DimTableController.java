c2ee20ac652f78967a51b6b36f754eb7dbdad7bf
GloryRoadWang <tyutwang@gmail.com>
2015-05-15 14:00:26 +0800

    新建动态查询统计所有表清单Restful接口DimTableController

---
+++ src/main/java/gov/gwssi/csc/scms/controller/statistics/DimTableController.java
@@ -0,0 +1,34 @@
+package gov.gwssi.csc.scms.controller.statistics;
+
+import gov.gwssi.csc.scms.service.statistics.DimTableService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by WangZhenghua on 2015/5/15.
+ * 动态查询统计所有表清单Restful接口
+ */
+@RestController
+@RequestMapping("/statistics")
+public class DimTableController {
+    @Autowired
+    private DimTableService dimTableService;
+
+    private final String RETURN_BLANK_JSON = "[]";
+
+    // 获取资源-动态查询统计所有表清单
+    @RequestMapping(value="dimtable",method = RequestMethod.GET, headers = "Accept=application/json;charset=utf-8")
+    public String getAllDimTableJsonData(){
+        String dimTableJsonData = "";
+        try{
+            dimTableJsonData = dimTableService.getAllDimTableJsonData();
+        }catch (Exception e){
+            e.printStackTrace();
+            return RETURN_BLANK_JSON;
+        }
+        return dimTableJsonData;
+    }
+
+}
