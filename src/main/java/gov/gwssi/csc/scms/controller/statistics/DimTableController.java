abde9579a2db02091b8be6c8489e665deb81b703
GloryRoadWang <tyutwang@gmail.com>
2015-05-15 16:06:27 +0800

    DimTableController添加httpResponse.setHeader("Access-Control-Allow-Origin", "*");

--- src/main/java/gov/gwssi/csc/scms/controller/statistics/DimTableController.java
+++ src/main/java/gov/gwssi/csc/scms/controller/statistics/DimTableController.java
@@ -1,10 +1,14 @@
 package gov.gwssi.csc.scms.controller.statistics;
 
+import com.sun.deploy.net.HttpResponse;
 import gov.gwssi.csc.scms.service.statistics.DimTableService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by WangZhenghua on 2015/5/15.
@@ -20,7 +24,9 @@ public class DimTableController {
 
     // 获取资源-动态查询统计所有表清单
     @RequestMapping(value="dimtable",method = RequestMethod.GET, headers = "Accept=application/json;charset=utf-8")
-    public String getAllDimTableJsonData(){
+    public String getAllDimTableJsonData(HttpServletResponse httpResponse){
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+//      httpResponse.setStatus(503);
         String dimTableJsonData = "";
         try{
             dimTableJsonData = dimTableService.getAllDimTableJsonData();
