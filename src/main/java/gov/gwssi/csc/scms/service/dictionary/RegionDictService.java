93248a8aad4f10527f15af703f7f55d259d987a4
GloryRoadWang <tyutwang@gmail.com>
2015-06-01 14:22:20 +0800

    字典表service层修改

--- src/main/java/gov/gwssi/csc/scms/service/dictionary/RegionDictService.java
+++ src/main/java/gov/gwssi/csc/scms/service/dictionary/RegionDictService.java
@@ -1,8 +1,11 @@
 package gov.gwssi.csc.scms.service.dictionary;
 
 import gov.gwssi.csc.scms.dao.dictionary.RegionDictDAO;
+import gov.gwssi.csc.scms.domain.dictionary.DictTreeJson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by WangZhenghua on 2015/4/23.
@@ -16,11 +19,13 @@ public class RegionDictService {
     private RegionDictDAO regionDictDAO;
 
     // 根据region层次获取相应的大洲以及国别信息
-    public String getRegionDictJsonData(String level){
-        String jsonData = "[]";
-        jsonData = regionDictDAO.getRegionDictJsonDataByLevel(level);
-        System.out.println("RegionDictService-->getRegionDictJsonData方法jsonData == " + jsonData);
-        return jsonData;
+    public List<DictTreeJson> getRegionDictTreeByLevel(String level) throws  NoSuchDictTreeException{
+         List<DictTreeJson> regionDictTree = null;
+         regionDictTree = regionDictDAO.getRegionDictByLevel(level);
+         if(regionDictTree == null){
+             throw new NoSuchDictTreeException("can not find the region with level " +level);
+         }
+         return  regionDictTree;
     }
 
 }
--- src/main/java/gov/gwssi/csc/scms/service/dictionary/TranslateDictService.java
+++ src/main/java/gov/gwssi/csc/scms/service/dictionary/TranslateDictService.java
@@ -1,8 +1,11 @@
 package gov.gwssi.csc.scms.service.dictionary;
 
 import gov.gwssi.csc.scms.dao.dictionary.TranslateDictDAO;
+import gov.gwssi.csc.scms.domain.dictionary.TranslateDictJson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by WangZhenghua on 2015/4/27.
@@ -15,11 +18,13 @@ public class TranslateDictService {
     private TranslateDictDAO translateDictDAO;
 
     // 根据classId获取相应的代码表
-    public String getTranslateDictJsonData(String classId) {
-        String jsonData = "[]";
-        jsonData = translateDictDAO.getTranslateDictJsonDataByClassId(classId);
-        System.out.println("TranslateDictService --> getTranslateDictJsonData方法jsonData= "+jsonData);
-        return jsonData;
+    public List<TranslateDictJson> getTranslateDictByClassId(String classId) throws NoSuchDictTreeException{
+        List<TranslateDictJson> list = null;
+        list = translateDictDAO.getTranslateDictByClassId(classId);
+        if(list == null){
+            throw new NoSuchDictTreeException("can not find translate dict by classId with "+classId);
+        }
+        return list;
     }
 
 }
