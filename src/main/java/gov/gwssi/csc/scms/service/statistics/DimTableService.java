83a82a6dd2a72985e277a0a0a4659a94fb17241f
GloryRoadWang <tyutwang@gmail.com>
2015-06-01 14:17:08 +0800

    DimTableService类添加获取tables及tables下的字段列表方法

--- src/main/java/gov/gwssi/csc/scms/service/statistics/DimTableService.java
+++ src/main/java/gov/gwssi/csc/scms/service/statistics/DimTableService.java
@@ -1,7 +1,10 @@
 package gov.gwssi.csc.scms.service.statistics;
 
 import gov.gwssi.csc.scms.dao.statistics.DimTableDAO;
+import gov.gwssi.csc.scms.dao.statistics.TablesDAO;
+import gov.gwssi.csc.scms.domain.statistics.TablesJson;
 import gov.gwssi.csc.scms.service.BaseService;
+import gov.gwssi.csc.scms.service.dictionary.NoSuchDictTreeException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +21,21 @@ public class DimTableService extends BaseService {
     @Autowired
     private DimTableDAO dimTableDAO;
 
+    @Autowired
+    private TablesDAO tablesDAO;
+
     public String getAllDimTableJsonData(){
         return dimTableDAO.getAllDimTableJsonData();
     }
+
+    // 获取tables及tables下的字段列表
+    public List<TablesJson> getTables() throws NoSuchDictTreeException{
+        List<TablesJson> tablesJson = null;
+        tablesJson = tablesDAO.getTables();
+        if(tablesJson == null){
+            throw new NoSuchDictTreeException("can not find the tablesJsons");
+        }
+        return tablesJson;
+    }
+
 }
