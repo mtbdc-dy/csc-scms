f02823166ebd76a4019b03b829adeb45b651b64e
Wang Zishi <ynh.2@outlook.com>
2015-10-15 18:33:53 +0800

    结果集加入序号

--- src/main/java/gov/gwssi/csc/scms/domain/dynamicReport/Report/Report.java
+++ src/main/java/gov/gwssi/csc/scms/domain/dynamicReport/Report/Report.java
@@ -38,7 +38,7 @@ public class Report {
         this.body = new ArrayList<Row>();
         for (Map rowMap : bodyMaps) {
             row = new Row();
-            for (Integer i = 1; i <= rowMap.size(); i++) {
+            for (Integer i = 0; i < rowMap.size(); i++) {
                 row.add(new Cell(1,1, rowMap.get("COL" + i.toString()).toString()));
             }
             this.body.add(row);
